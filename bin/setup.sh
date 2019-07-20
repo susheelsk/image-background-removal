037cee44f626dcba31fbdc0f13640f3cd7edb793
Anodev <29470622+OPHoperHPO@users.noreply.github.com>
2019-07-20 16:53:19 +1000

    Fix issue

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,9 @@
+mkdir bin/mobile_net_model
+mkdir bin/xception_model
+cd bin
 wget http://download.tensorflow.org/models/deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz
 wget http://download.tensorflow.org/models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz
 
-mkdir mobile_net_model
-mkdir xception_model
 tar xvzf deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz -C mobile_net_model --strip=1
 tar xvzf deeplabv3_pascal_train_aug_2018_01_04.tar.gz -C xception_model --strip=1
 
