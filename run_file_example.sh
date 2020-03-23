90735f23a82b254b839ffd8fe01560ff4520b712
Anodev <29470622+OPHoperHPO@users.noreply.github.com>
2020-03-23 11:11:20 +1000

    Update run_file_example.sh

--- run_file_example.sh
+++ run_file_example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The file will be saved along the path ./docs/imgs/output/1.png
-python main.py ./docs/imgs/input/1.jpg ./docs/imgs/output/1.png xception_model
+python3 main.py ./docs/imgs/input/1.jpg ./docs/imgs/output/1.png xception_model
