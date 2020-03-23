f0a5af9114bf7a6aa6501217faf54e794ebcf555
Anodev <29470622+OPHoperHPO@users.noreply.github.com>
2020-03-23 11:11:37 +1000

    Update run_file_example.bat

--- run_file_example.bat
+++ run_file_example.bat
@@ -1,2 +1,2 @@
 Rem The file will be saved along the path .\docs\imgs\output\1.png
-python main.py .\docs\imgs\input\1.jpg .\docs\imgs\output\1.png xception_model
+python3 main.py .\docs\imgs\input\1.jpg .\docs\imgs\output\1.png xception_model
