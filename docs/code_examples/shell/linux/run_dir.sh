43c9fe85f2247257e34b7326e554e88fc4212fd1
Nikita Selin (Anodev) <farvard34@gmail.com>
2022-05-10 07:02:21 +1000

    Updated code examples

--- docs/code_examples/shell/linux/run_dir.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# All files in the input directory will be saved in the output folder under their names with the extension .png.
-# When replacing folder paths, ALWAYS add / (or \ in the case of Windows) at the end!
-python3 main.py -i ./docs/imgs/input/ -o ./output/ -m u2net
